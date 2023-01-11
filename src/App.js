7f4833a95407499f8e5bf8d974aad0217dee3351
Milena Diniz Vieira <miladvieira@outlook.com>
2023-01-11 17:31:02 -0300

    Implementado api do MELI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getCategories } from './services/api';
 
 function App() {
+  getCategories().then((response) => console.log(response));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Iniciando projeto</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
