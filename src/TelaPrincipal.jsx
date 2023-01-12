48083e5d174bbe0d86e71836762ad750c157e03f
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-01-12 15:54:49 -0300

    renderizando categorias

--- src/TelaPrincipal.jsx
+++ src/TelaPrincipal.jsx
@@ -1,13 +1,31 @@
 import { Component } from 'react';
+import Categorias from './Categorias';
+import { getCategories } from './services/api';
 
 class TelaPrincipal extends Component {
+  state = {
+    categoria: [],
+  };
+
+  componentDidMount() {
+    getCategories()
+      .then((response) => this.setState({
+        categoria: response,
+      }));
+  }
+
   render() {
+    const { categoria } = this.state;
     return (
       <div>
         <input type="text" />
         <p data-testid="home-initial-message">
           digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Categorias
+          categories={ categoria }
+          // {/*selectCategories={}*/}
+        />
       </div>
     );
   }
