9889b14d3102a18f609a6f39b3e815bfed91f380
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2023-01-12 16:15:35 -0300

    recriando pages

---
+++ src/pages/Carrinho.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Carrinho extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </p>
+      </div>
+    );
+  }
+}
+export default Carrinho;
