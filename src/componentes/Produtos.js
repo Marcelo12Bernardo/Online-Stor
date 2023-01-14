9ccfd4b18ab1b314dd3e599cf619a83afa8ac4ad
FabianoCarvalho <fabianolucas27@hotmail.com>
2023-01-13 21:30:54 -0400

    Requisito 08 incompleto

--- src/componentes/Produtos.js
+++ src/componentes/Produtos.js
@@ -3,14 +3,29 @@ import PropTypes from 'prop-types';
 
 export default class Produtos extends Component {
   render() {
-    const { produtos } = this.props;
+    const { produtos, onClick, testId, quantityId } = this.props;
     return (
       <div>
         {produtos.map((produto) => (
-          <div key={ produto.id } data-testid="product">
+          <div key={ produto.id }>
+            {/* <p >
+              Produto
+            </p> */}
+            <p data-testid={ quantityId }>
+              0
+            </p>
             <img src={ produto.thumbnail } alt={ produto.title } />
-            <p>{ produto.title }</p>
+            <p data-testid={ testId }>
+              { produto.title }
+            </p>
             <p>{ produto.price }</p>
+            <button
+              type="button"
+              data-testid="product-add-to-cart"
+              onClick={ () => onClick(produto.id) }
+            >
+              salvar
+            </button>
           </div>
         ))}
       </div>
@@ -18,6 +33,9 @@ export default class Produtos extends Component {
   }
 }
 Produtos.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  quantityId: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
   produtos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string,
     title: PropTypes.string,
--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,12 +1,50 @@
 import { Component } from 'react';
+import Produtos from '../componentes/Produtos';
 
 class Carrinho extends Component {
+  state = {
+    itensCarrinho: [],
+  };
+
+  componentDidMount() {
+    this.getItemsLocalStorage();
+  }
+
+  emptyCard = () => (
+    <p data-testid="shopping-cart-empty-message">
+      Seu carrinho está vazio
+    </p>
+  );
+
+  getItemsLocalStorage = () => {
+    if (localStorage.getItem('key')) {
+      const carrinho = JSON.parse(localStorage.getItem('key'));
+      this.setState({
+        itensCarrinho: carrinho,
+      });
+    }
+  };
+
   render() {
+    const { itensCarrinho } = this.state;
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">
-          Seu carrinho está vazio
-        </p>
+        {this.emptyCard()}
+        {
+          itensCarrinho.map(({ id, quantity, thumbnail, title, price }) => (
+            <div key={ id }>
+              <p data-testid="shopping-cart-product-quantity">
+                0
+                { quantity }
+              </p>
+              <img src={ thumbnail } alt={ title } />
+              <p data-testid="shopping-cart-product-name">
+                { title }
+              </p>
+              <p>{ price }</p>
+            </div>
+          ))
+        }
       </div>
     );
   }
