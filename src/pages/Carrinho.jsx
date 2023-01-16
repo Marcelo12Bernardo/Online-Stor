5404a978b5787af254937774df9684fcaa53d66a
FabianoCarvalho <fabianolucas27@hotmail.com>
2023-01-16 18:05:20 -0400

    Requisito 10

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -30,18 +30,72 @@ class Carrinho extends Component {
     return (
       itensCarrinho.map(({ id, quantity, thumbnail, title, price }) => (
         <div key={ id }>
-          <p data-testid="shopping-cart-product-quantity">
-            1
-            { quantity }
-          </p>
           <img src={ thumbnail } alt={ title } />
           <p data-testid="shopping-cart-product-name">
             { title }
           </p>
           <p>{ price }</p>
+          <div key={ id }>
+            <button
+              data-testid="remove-product"
+              id={ id }
+              type="button"
+              onClick={ () => this.removeItensCart(id) }
+            >
+              x
+            </button>
+            <span data-testid="shopping-cart-product-quantity">
+              <button
+                id={ id }
+                data-testid="product-decrease-quantity"
+                type="button"
+                onClick={ this.decreaseProduct }
+              >
+                -
+              </button>
+              { quantity }
+              <button
+                id={ id }
+                data-testid="product-increase-quantity"
+                type="button"
+                onClick={ this.increaseProduct }
+              >
+                +
+              </button>
+            </span>
+          </div>
         </div>
       ))
     );
+  };
+
+  removeItensCart = (id) => {
+    this.setState((prevState) => ({ itensCarrinho: prevState.itensCarrinho
+      .filter((produto) => (produto.id !== id)),
+    }), () => {
+      const { itensCarrinho } = this.state;
+      localStorage.setItem('key', JSON.stringify(itensCarrinho));
+    });
+  };
+
+  increaseProduct = ({ target }) => {
+    const { itensCarrinho } = this.state;
+    const produto1 = itensCarrinho.find((produto) => produto.id === target.id);
+    if (produto1.quantity < produto1.available_quantity) {
+      produto1.quantity += 1;
+    }
+    this.setState({ itensCarrinho: [...itensCarrinho] });
+    localStorage.setItem('key', JSON.stringify(itensCarrinho));
+  };
+
+  decreaseProduct = ({ target }) => {
+    const { itensCarrinho } = this.state;
+    const produto1 = itensCarrinho.find((produto) => produto.id === target.id);
+    if (produto1.quantity > 1) {
+      produto1.quantity -= 1;
+    }
+    this.setState({ itensCarrinho: [...itensCarrinho] });
+    localStorage.setItem('key', JSON.stringify(itensCarrinho));
   };
 
   render() {
